1ec1f7c6255e49f30a8e1ef820519a11724ea8a6
yevgeniy.shaidur <insight2pizzza@gmail.com>
2021-11-27 17:40:03 +0200

    upd

--- src/main/java/com/crudhttp/app/controller/UserController.java
+++ src/main/java/com/crudhttp/app/controller/UserController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/user")
+@WebServlet(urlPatterns = "/user", name="UserController")
 public class UserController extends HttpServlet {
     private final UserServiceImpl userService;
 
@@ -19,12 +19,11 @@ public class UserController extends HttpServlet {
         userService = new UserServiceImpl();
     }
 
-    public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        response.setContentType ("text/html");
-        PrintWriter out = response.getWriter ();
-        out.print ("<html><body>");
-        out.print ("<h3>Hello Servlet</h3>");
-        out.print ("</body></html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<html><body>");
+        out.print("<h3>Hello Servlet</h3>");
+        out.print("</body></html>");
     }
 }
