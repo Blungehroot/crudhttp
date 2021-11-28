19eddb17c1362f92145dfa3bc4bad3cf15cc57f4
yevgeniy.shaidur <insight2pizzza@gmail.com>
2021-11-28 15:52:20 +0200

    user controller impl

--- src/main/java/com/crudhttp/app/controller/UserController.java
+++ src/main/java/com/crudhttp/app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.crudhttp.app.controller;
 
 import com.crudhttp.app.model.User;
 import com.crudhttp.app.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,21 +10,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/user", name="UserController")
+@WebServlet(urlPatterns = {"/users", "/users/get", "/users/get-all"}, name = "UserController")
 public class UserController extends HttpServlet {
     private final UserServiceImpl userService;
+    private static ObjectMapper om = new ObjectMapper();
 
     public UserController() {
         userService = new UserServiceImpl();
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.print("<html><body>");
-        out.print("<h3>Hello Servlet</h3>");
-        out.print("</body></html>");
+    private void getById(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int userId = Integer.parseInt(req.getParameter("id"));
+        userService.getById(userId);
+        resp.setContentType("application/json");
+        resp.getWriter().write(om.writeValueAsString(userService.getById(userId)));
+    }
+
+    private void getAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().write(om.writeValueAsString(userService.getAll()));
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("name");
+        User user = new User();
+        user.setName(userName);
+        userService.save(user);
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+        resp.setContentType("application/json");
+        resp.getWriter().write(om.writeValueAsString(user));
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        userService.deleteById(userService.getById(id).getId());
+    }
+
+    @Override
+    public void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int userId = Integer.parseInt(req.getParameter("id"));
+        String userName = req.getParameter("name");
+        User user = new User();
+        user.setId(userId);
+        user.setName(userName);
+        userService.update(user);
+        resp.setContentType("application/json");
+        resp.getWriter().write(om.writeValueAsString(user));
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getServletPath();
+        switch (action) {
+            case "/users/get":
+                getById(req, resp);
+                break;
+            case "/users/get-all":
+                getAll(req, resp);
+                break;
+        }
     }
 }
