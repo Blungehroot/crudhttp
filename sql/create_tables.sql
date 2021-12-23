04d7b204d9d80b0e8b86d1daad5feea3590e4143
yevgeniy.shaidur <insight2pizzza@gmail.com>
2021-12-23 20:16:46 +0200

    add_variables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -27,23 +27,3 @@ CREATE TABLE IF NOT EXISTS events
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE
 );
-
-/*CREATE TABLE IF NOT EXISTS user_events
-(
-    user_id INT NOT NULL,
-    event_id  INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES events (id) ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS event_medias
-(
-    event_id   INT NOT NULL,
-    media_id INT NOT NULL,
-    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
-    FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES events (id) ON UPDATE CASCADE,
-    FOREIGN KEY (media_id) REFERENCES media (id) ON UPDATE CASCADE
-);*/
